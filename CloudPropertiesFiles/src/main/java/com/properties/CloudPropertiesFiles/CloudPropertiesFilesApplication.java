6adc7a44f039513e4b54a95acdcf1d0bb0deba62
kiranbosco <31914949+kiranbosco@users.noreply.github.com>
2017-11-23 22:13:54 +0530

    Removed Document

--- CloudPropertiesFiles/src/main/java/com/properties/CloudPropertiesFiles/CloudPropertiesFilesApplication.java
+++ CloudPropertiesFiles/src/main/java/com/properties/CloudPropertiesFiles/CloudPropertiesFilesApplication.java
@@ -4,11 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-/**
- * 
- * @author puriniBosco
- *
- */
+
 @SpringBootApplication
 @EnableConfigServer
 public class CloudPropertiesFilesApplication {

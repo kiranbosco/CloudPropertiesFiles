8190496f6c36190cc02ab1286adb5acb12465ce2
kiranbosco <31914949+kiranbosco@users.noreply.github.com>
2017-12-12 21:29:58 +0530

    Project added

---
+++ IocTesting/src/main/java/com/example/demo/IocTestingApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IocTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IocTestingApplication.class, args);
+	}
+}
